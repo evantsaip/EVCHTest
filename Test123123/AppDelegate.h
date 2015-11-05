e10cb31b55cace610672f165f356f9f49203a93b
Evan <evan.tsai@tgc-taiwan.com.tw>
2015-11-05 14:54:17 +0800

    firstcommit

---
+++ Test123123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test123123
+//
+//  Created by Evan on 2015/11/5.
+//  Copyright © 2015年 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test123123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test123123
+//
+//  Created by Evan on 2015/11/5.
+//  Copyright © 2015年 Evan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
